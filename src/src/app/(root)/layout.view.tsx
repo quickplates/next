4e4f80cfa584b2456b4ed39a1b33bdab955362cf
Sebastian Pietras <me@spietras.dev>
2026-06-05 22:36:39 +0200

    Added identity and access features (#43)

--- src/src/app/(root)/layout.view.tsx
+++
@@ -1,9 +0,0 @@
-import type { LayoutViewInput } from "../types";
-import type { Schemas } from "./schemas";
-import type { Keys } from "./types";
-
-export async function RootLayoutView({
-  children,
-}: LayoutViewInput<typeof Schemas.Path, Keys.Slots>) {
-  return children;
-}
